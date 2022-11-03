30e4905b28ee4bd2f0d63ebfcc2a1ad9164addfd
saymanrabbi <mdrabbi.asm@gmail.com>
2022-11-03 12:11:57 +0600

    fetchProduct

--- src/redux/product.action.js
+++ src/redux/product.action.js
@@ -5,3 +5,8 @@ export const fetchProducts = () => async (dispatch) => {
     const response = await fakeStoreApi.get("/products");
     dispatch({ type: actionType.FETCH_PRODUCTS, payload: response.data });
 }
+
+export const fetchProduc = (id)=> async (dispatch)=>{
+    const response = await fakeStoreApi.get(`/products/${id}`);
+    dispatch({type:actionType.SELECTED_PRODUCT,payload:response.data})
+}
