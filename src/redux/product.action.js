9234fd28171b063ba47fd1dabf28e887e24a5378
saymanrabbi <mdrabbi.asm@gmail.com>
2022-11-03 12:22:15 +0600

    done with redux thunk

--- src/redux/product.action.js
+++ src/redux/product.action.js
@@ -6,7 +6,26 @@ export const fetchProducts = () => async (dispatch) => {
     dispatch({ type: actionType.FETCH_PRODUCTS, payload: response.data });
 }
 
-export const fetchProduc = (id)=> async (dispatch)=>{
+export const fetchProduct = (id)=> async (dispatch)=>{
     const response = await fakeStoreApi.get(`/products/${id}`);
     dispatch({type:actionType.SELECTED_PRODUCT,payload:response.data})
 }
+export const setProducts = (products) => {
+    return {
+      type: actionType.SET_PRODUCTS,
+      payload: products,
+    };
+  };
+  
+  export const selectedProduct = (product) => {
+    return {
+      type: actionType.SELECTED_PRODUCT,
+      payload: product,
+    };
+  };
+  
+  export const removeSelectedProduct = () => {
+    return {
+      type: actionType.REMOVE_SELECTED_PRODUCT,
+    };
+  };
